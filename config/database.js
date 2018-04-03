85b002430b99a1756d683b448fe4c26fe736d3b3
Diego Fernandes <diego.schell.f@gmail.com>
2018-04-03 17:19:06 -0300

    Variáveis ambiente com DotEnv

--- config/database.js
+++ config/database.js
@@ -1,23 +1,9 @@
+require('dotenv').config();
+
 module.exports = {
-  development: {
-    username: 'root',
-    password: null,
-    database: 'nodejs_mod02',
-    host: '127.0.0.1',
-    dialect: 'mysql',
-  },
-  test: {
-    username: 'root',
-    password: null,
-    database: 'database_test',
-    host: '127.0.0.1',
-    dialect: 'mysql',
-  },
-  production: {
-    username: 'root',
-    password: null,
-    database: 'database_production',
-    host: '127.0.0.1',
-    dialect: 'mysql',
-  },
+  username: process.env.DATABASE_USER || 'root',
+  password: process.env.DATABASE_PASS || null,
+  database: process.env.DATABASE_NAME || 'database',
+  host: process.env.DATABASE_HOST || '127.0.0.1',
+  dialect: 'mysql',
 };

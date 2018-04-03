fdb13a5fa0a012af469adb9af7b5609a3f040447
Diego Fernandes <diego.schell.f@gmail.com>
2018-04-03 17:21:28 -0300

    Configurando rotas e Insomnia

--- index.js
+++ index.js
@@ -2,10 +2,14 @@ require('dotenv').config();
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const routes = require('./app/routes');
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
+// Routes
+app.use('/', routes);
 
 app.listen(process.env.PORT || 3000);

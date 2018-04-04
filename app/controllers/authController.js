0cc6d85d57b537a871de1e34fbde3f052b4abd82
Diego Fernandes <diego.schell.f@gmail.com>
2018-04-04 12:06:56 -0300

    Criação de conta

--- app/controllers/authController.js
+++ app/controllers/authController.js
@@ -1,4 +1,5 @@
-// const { User } = require('../models');
+const { User } = require('../models');
+const bcrypt = require('bcryptjs');
 
 module.exports = {
   signin(req, res) {
@@ -7,5 +8,24 @@ module.exports = {
 
   signup(req, res) {
     return res.render('auth/signup', { layout: '_layouts/auth' });
+  },
+
+  async register(req, res) {
+    try {
+      const { email } = req.body;
+
+      if (await User.findOne({ where: { email } })) {
+        return console.log('E-mail já cadastrado');
+      }
+
+      const password = await bcrypt.hash(req.body.password, 5);
+
+      await User.create({ ...req.body, password });
+
+      return res.redirect('/');
+    } catch (err) {
+      console.log(err);
+      return res.send('/signup');
+    }
   },
 };

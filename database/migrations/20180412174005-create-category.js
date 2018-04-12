2534b8238c7c16b946445cac9d64362a8d5d07f0
Diego Fernandes <diego.schell.f@gmail.com>
2018-04-12 14:42:44 -0300

    Model & migration de categoria

---
+++ database/migrations/20180412174005-create-category.js
@@ -0,0 +1,35 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    const CategoriesTable = queryInterface.createTable('Categories', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        references: { model: 'Users', key: 'id' },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+        allowNull: false,
+      },
+      title: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+
+    return CategoriesTable;
+  },
+
+  down: queryInterface => queryInterface.dropTable('Categories'),
+};
